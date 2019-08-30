0862fdb5e86839167b9bb8ac1fc674868e3238f7
Paul Jones <wpnpaul@live.unc.edu>
2019-08-30 14:06:43 -0400

    Added solution to A1Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,28 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int numCustomers = scan.nextInt();
+		String orderOutputs[] = new String[numCustomers];
 		
+		for (int i = 0; i < numCustomers; i++) {
+			String firstName = scan.next();
+			String name = firstName.substring(0, 1) + ". " + scan.next();
+			int numOfItems = scan.nextInt();
+			
+			double total = 0.0;
+			for (int j = 0; j < numOfItems; j++) {
+				int quantity = scan.nextInt();
+				String item = scan.next();
+				double price = scan.nextDouble();
+				
+				total = total + quantity * price;
+			}
+			String customerOrderTotal = name + ": " + String.format("%.2f", total);
+			orderOutputs[i] = customerOrderTotal;
+		}
+
+		for (int i = 0; i < numCustomers; i++) {
+			System.out.println(orderOutputs[i]);
+		}
 	}
 }
