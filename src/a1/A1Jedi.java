191ee15144123b2caaf07a5867bc9b43544a52b5
Paul Jones <wpnpaul@live.unc.edu>
2019-09-02 00:32:44 -0400

    Added solution to A1Jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,42 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int numItems = scan.nextInt();
+		String itemCatelog[] = new String[numItems];
+		int itemQuantity[] = new int[numItems];
+		int numCustomersBoughtItem[] = new int[numItems];
 		
+		for (int i = 0; i < numItems; i++) {
+			String item = scan.next();
+			String price = scan.next();
+			itemCatelog[i] = item;
+		}
+		
+		int numCustomers = scan.nextInt();
+		
+		for (int i = 0; i < numCustomers; i++) {
+			String name = scan.next() + " " + scan.next();
+			int customerItems = scan.nextInt();
+			
+			for (int j = 0; j < customerItems; j++) {
+				int quantity = scan.nextInt();
+				String item = scan.next();
+				for (int k = 0; k < numItems; k++) {
+					if (itemCatelog[k].equals(item)) {
+						itemQuantity[k] = quantity;
+						numCustomersBoughtItem[k]++;
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < itemCatelog.length; i++) {
+			if (itemQuantity[i] == 0) {
+				System.out.println("No customers bought " + itemCatelog[i]);
+			}
+			else {
+				System.out.println(numCustomersBoughtItem[i] + " customers bought " + itemQuantity[i] + " " + itemCatelog[i]);
+			}
+		}
 	}
 }
